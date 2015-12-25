de13c78db526fb678baaf84be1656dc9941a32f8
Thangiee <Thangiee0@gmail.com>
2015-12-25 14:33:21 -0600

    refactor account api routes

--- src/main/scala/com/utamars/api/AccountApi.scala
+++ src/main/scala/com/utamars/api/AccountApi.scala
@@ -14,59 +14,53 @@ case class AccountApi(implicit ec: ExecutionContext, sm: SessMgr, rts: RTS) exte
   override val defaultAuthzRoles = Seq(Role.Admin, Role.Instructor, Role.Assistant)
 
   override val route =
-    pathPrefix("account") {
-      (post & path("create"/"assistant")) {
-        formFields('netid, 'user, 'pass, 'email, 'rate.as[Double], 'job,
-          'dept, 'first, 'last, 'empid, 'title, 'titlecode).as(CreateAssistantForm) { form =>
-          Account.createFromForm(form.copy(pass = form.pass.bcrypt)).responseWith(OK)
-        }
-      } ~
-      (post & path("create"/"instructor")) {
-        formFields('netid, 'user, 'pass, 'email, 'first, 'last).as(CreateInstructorAccForm) { form =>
-          Account.createFromForm(form.copy(pass = form.pass.bcrypt)).responseWith(OK)
-        }
-      } ~
-      (get & path("info") & authnAndAuthz()) { (acc) =>
-        Account.findBy(acc.username).responseWith(acc => acc.copy(passwd = "").toJson.compactPrint) // hide password
-      } ~
-      (get & path("info"/Segment) & authnAndAuthz(Role.Admin, Role.Instructor)) { (username, _) =>
-        Account.findBy(username).responseWith(acc => acc.copy(passwd = "").toJson.compactPrint)
-      } ~
-      (post & path("change-password") & authnAndAuthz()) { acc =>
-        formField('newpassword) { newPass =>
-          acc.changePassword(newPass.bcrypt).responseWith(OK)
-        }
-      } ~
-      (post & path("change-password"/Segment) & authnAndAuthz(Role.Admin)) { (username, _) =>
-        formField('newpassword) { newPass =>
-          Account.changePassword(username, newPass.bcrypt).responseWith(OK)
-        }
+    ((post|put) & path("account"/"assistant")) {
+      formFields('netid, 'user, 'pass, 'email, 'rate.as[Double], 'job,
+        'dept, 'first, 'last, 'empid, 'title, 'titlecode).as(CreateAssistantForm) { form =>
+        Account.createFromForm(form.copy(pass = form.pass.bcrypt)).responseWith(OK)
       }
     } ~
-    pathPrefix("assistant") {
-      (get & path("info") & authnAndAuthz(Role.Assistant)) { acc =>
-        Assistant.findBy(acc.netId).responseWith(asst => asst.toJson.compactPrint)
-      } ~
-      (get & path("info"/Segment) & authnAndAuthz(Role.Instructor, Role.Admin)) { (netId, _) =>
-        Assistant.findBy(netId).responseWith(asst => asst.toJson.compactPrint)
-      } ~
-      (post & path("update") & authnAndAuthz(Role.Assistant)) { acc =>
-        formFields('rate.as[Double].?, 'dept.?, 'title.?, 'titlecode.?).as(UpdateAssistantForm) { form =>
-          Assistant.update(acc.netId, form).responseWith(OK)
-        }
+    ((post|put) & path("account"/"instructor")) {
+      formFields('netid, 'user, 'pass, 'email, 'first, 'last).as(CreateInstructorAccForm) { form =>
+        Account.createFromForm(form.copy(pass = form.pass.bcrypt)).responseWith(OK)
       }
     } ~
-    pathPrefix("instructor") {
-      (get & path("info") & authnAndAuthz(Role.Instructor)) { acc =>
-        Instructor.findBy(acc.netId).responseWith(inst => inst.toJson.compactPrint)
-      } ~
-      (get & path("info"/Segment) & authnAndAuthz(Role.Admin)) { (netId, _) =>
-        Instructor.findBy(netId).responseWith(inst => inst.toJson.compactPrint)
-      } ~
-      (post & path("update") & authnAndAuthz(Role.Instructor)) { acc =>
-        formFields('email.?, 'lastname.?, 'firstname.?).as(UpdateInstructorForm) { form =>
-          Instructor.update(acc.netId, form).responseWith(OK)
-        }
+    (get & path("account") & authnAndAuthz()) { (acc) =>
+      Account.findBy(acc.username).responseWith(acc => acc.copy(passwd = "").toJson.compactPrint) // hide password
+    } ~
+    (get & path("account"/Segment) & authnAndAuthz(Role.Admin, Role.Instructor)) { (username, _) =>
+      Account.findBy(username).responseWith(acc => acc.copy(passwd = "").toJson.compactPrint)
+    } ~
+    ((post|put) & path("account"/"change-password") & authnAndAuthz()) { acc =>
+      formField('newpassword) { newPass =>
+        acc.changePassword(newPass.bcrypt).responseWith(OK)
+      }
+    } ~
+    ((post|put) & path("account"/"change-password"/Segment) & authnAndAuthz(Role.Admin)) { (username, _) =>
+      formField('newpassword) { newPass =>
+        Account.changePassword(username, newPass.bcrypt).responseWith(OK)
+      }
+    } ~
+    (get & path("assistant") & authnAndAuthz(Role.Assistant)) { acc =>
+      Assistant.findBy(acc.netId).responseWith(asst => asst.toJson.compactPrint)
+    } ~
+    (get & path("assistant"/Segment) & authnAndAuthz(Role.Instructor, Role.Admin)) { (netId, _) =>
+      Assistant.findBy(netId).responseWith(asst => asst.toJson.compactPrint)
+    } ~
+    ((post|put) & path("assistant") & authnAndAuthz(Role.Assistant)) { acc =>
+      formFields('rate.as[Double].?, 'dept.?, 'title.?, 'titlecode.?).as(UpdateAssistantForm) { form =>
+        Assistant.update(acc.netId, form).responseWith(OK)
+      }
+    } ~
+    (get & path("instructor") & authnAndAuthz(Role.Instructor)) { acc =>
+      Instructor.findBy(acc.netId).responseWith(inst => inst.toJson.compactPrint)
+    } ~
+    (get & path("instructor"/Segment) & authnAndAuthz(Role.Admin)) { (netId, _) =>
+      Instructor.findBy(netId).responseWith(inst => inst.toJson.compactPrint)
+    } ~
+    ((post|put) & path("instructor") & authnAndAuthz(Role.Instructor)) { acc =>
+      formFields('email.?, 'lastname.?, 'firstname.?).as(UpdateInstructorForm) { form =>
+        Instructor.update(acc.netId, form).responseWith(OK)
       }
     }
 }
